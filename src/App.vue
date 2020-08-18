3b1d0589cb0b3cee2d11579bd5af9a0c50979cb2
Wanchai Nool <wanchainool@MacBook-Pro-khxng-Wanchai.local>
2020-08-18 10:38:22 +0700

    git

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ total }}</h1>
+    <div class="row">
+      <div class="col-3">
+        <counter lable="ok" @cc="sum" />
+      </div>
+      <div class="col-3">
+        <counter lable="ชอบ" @cc="sum" />
+      </div>
+      <div class="col-3">
+        <counter lable="อะหยังครับจารย์" @cc="sum" />
+      </div>
+      <div class="col-3">
+        <counter lable="55555" @cc="sum" />
+      </div>
+    </div>
+    ชื่อ-นามสกุล <input type="text" v-model="nam">  
+    รหัส นศ  <input type="text" v-model="code"/>
+    GPA <input type="number" v-model="gpa" step="0.01"/>
+    {{name}}{{code}}{{GPA}}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "/src/components/HelloWorld.vue";
+import Counter from "/src/components/Counter.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    Counter,
+  },
+
+  data: function () {
+    return {
+      total: 0,
+      name:'',
+      code:'',
+      GPA:0.0,
+    }
+  },
+  methods: {
+    sum(CounterValue) {
+      this.total += CounterValue;
+    },
+  },
 }
 </script>
 
---
+++ src/components/CsButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button @click="counter += 1">{{label}}</button>
+    <h1>{{counter}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CsButton",
+  props: {
+    label: String
+  },
+  data: function() {
+    return {
+      counter: 0,
+      classObject : {
+        active : true,
+        'text-danger' : false,
+        weight :20,
+      }
+    };
+  }
+};
+</script>
+
+<style>
+</style>
